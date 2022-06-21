420ba3d4838b8ccd6abe0fd6c681244f33122df6
s79379203 <76547452+s79379203@users.noreply.github.com>
2022-06-21 11:07:36 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -107,7 +107,7 @@ def handle_message(event):
                     )
                 ]
             )
-        )
+        
         # line_bot_api.reply_message(event.reply_token, carousel_template_message)
         template_message = TemplateSendMessage(
         alt_text = 'Carousel alt text', template = carousel_template)
